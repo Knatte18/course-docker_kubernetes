37141204dfbdfd15d7182f16eb24faf4b856cb5b
Henrik Anfinsen <henrik.anfinsen@gmail.com>
2024-07-14 09:03:28 +0200

    Changed app text

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          I was changed!
           Why do you do this???  <code>src/App.js</code> and save to reload.
         </p>
         <a
